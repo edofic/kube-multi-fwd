05b35c162385c3ee866f90039e99b27eec4f077e
Andraz Bajt <andraz@bajt.me>
2020-11-08 10:49:07 +0100

    Remove local proxy

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
@@ -22,7 +21,6 @@ type Command struct {
 }
 
 var commands = []Command{
-	{"proxy-test", "Quick proxying test", mainProxy},
 	{"server", "Server side that proxies traffic", mainServer},
 	{"client", "Client side that connects to the proxy", mainClient},
 }
@@ -51,46 +49,6 @@ func main() {
 	}
 	// command not found
 	printHelp(name)
-}
-
-func mainProxy() {
-	interfaceF := flag.String("interface", "127.0.0.1", "Interface to bind to")
-	portF := flag.Int("port", 63000, "Port to listen on")
-	flag.Parse()
-	address := fmt.Sprintf("%s:%d", *interfaceF, *portF)
-	log.Println("running proxy on", address)
-	listener, err := net.Listen("tcp", address)
-	if err != nil {
-		panic(err)
-	}
-	defer listener.Close()
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			panic(err)
-		}
-		go proxyConn(conn)
-	}
-}
-
-func proxyConn(conn net.Conn) {
-	target, err := net.Dial("tcp", "localhost:8000")
-	if err != nil {
-		log.Println("Error dialing", err)
-		return
-	}
-	closeCh := make(chan struct{}, 2)
-	go pipeConn(conn, target, closeCh)
-	go pipeConn(target, conn, closeCh)
-	<-closeCh
-	conn.Close()
-	target.Close()
-}
-
-func pipeConn(source, target net.Conn, closeCh chan struct{}) {
-	io.Copy(source, target)
-	//log.Print(n, err)
-	closeCh <- struct{}{}
 }
 
 func mainServer() {
